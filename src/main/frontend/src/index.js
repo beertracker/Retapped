350877d53b941d569f5bb45de6b47bdea926dc41
Bruk04 <iamnick2002@gmail.com>
2024-02-11 10:22:51 -0500

    Feat: Connection to Database

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -1,15 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css'
+  import { useState, useEffect } from 'react'
+  import { createClient } from '@supabase/supabase-js'
+  import { Auth } from '@supabase/auth-ui-react'
+  import { ThemeSupa } from '@supabase/auth-ui-shared'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  const supabase = createClient('https://<project>.supabase.co', '<your-anon-key>')
+
+  export default function App() {
+    const [session, setSession] = useState(null)
+
+    useEffect(() => {
+      supabase.auth.getSession().then(({ data: { session } }) => {
+        setSession(session)
+      })
+
+      const {
+        data: { subscription },
+      } = supabase.auth.onAuthStateChange((_event, session) => {
+        setSession(session)
+      })
+
+      return () => subscription.unsubscribe()
+    }, [])
+
+    if (!session) {
+      return (<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />)
+    }
+    else {
+      return (<div>Logged in!</div>)
+    }
+  }
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
