beed8af5a7c4df2c4a711b9a3fb369707751fc0f
Bruk04 <iamnick2002@gmail.com>
2024-02-11 15:28:29 -0500

    Feat: final fix for React

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import './index.js';
 
 function App() {
   return (
@@ -16,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         Learn React
+          Learn React
         </a>
       </header>
     </div>
