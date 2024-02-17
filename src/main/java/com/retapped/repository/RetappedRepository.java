b5b6e260cd3c894622a449e1aa0a242e4cb7996e
WRose226 <wjrose02@gmail.com>
2024-02-17 11:39:00 -0500

    Successfully Connected to DB
    Created Beer Entity, service, and repository.

---
+++ src/main/java/com/retapped/repository/RetappedRepository.java
@@ -0,0 +1,13 @@
+package com.retapped.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.retapped.domain.Beer;
+
+@Repository
+public interface RetappedRepository extends JpaRepository<Beer, Integer> {
+
+    // public Beer grabABeer();
+}
