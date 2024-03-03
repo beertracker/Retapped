65592adcc0debf1304928bfb9ba74841f284f5c4
grakehurst <143676692+grakehurst@users.noreply.github.com>
2024-03-03 14:14:41 -0500

    started coding the basic user UserProfile

---
+++ src/backend/UserProfile.java
@@ -0,0 +1,24 @@
+public class UserProfile {
+
+    //global fields
+    private String username; //username for the account; each is unique
+    private String password; //case sensitive password for the account
+
+
+    //default constructor
+    //creates a new user profile with null values
+    //values will be changes later
+    public userProfile() {
+        this.username = null;
+        this.password = null;
+    }
+
+    //constructor for the profile
+    //creates a profile with an input unique username and a password
+    public userProfile(String username, String password) {
+        
+
+
+    }
+
+}
